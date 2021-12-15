18011085be069a199abd1dd7564d9e8d760ddf72
Daniel-ossan <52332638+Daniel-ossan@users.noreply.github.com>
2021-12-15 20:14:21 -0300

    Delete ProjetoAuditorio directory

--- ProjetoAuditorio/src/main/java/com/ProjetoAuditorio/repositories/ParticipanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ProjetoAuditorio.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ProjetoAuditorio.models.Participante;
-
-public interface ParticipanteRepository extends CrudRepository<Participante, String> {
-
-}
